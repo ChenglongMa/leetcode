5579efe2c049b4f76dac1308a70fea4322fc4ce7
Chenglong Ma <mcl.cn@outlook.com>
2019-11-08 16:58:58 +1100

    Reformat code

--- 14.longest-common-prefix.py
+++ 14.longest-common-prefix.py
@@ -15,67 +15,70 @@
 #
 # Write a function to find the longest common prefix string amongst an array of
 # strings.
-# 
+#
 # If there is no common prefix, return an empty string "".
-# 
+#
 # Example 1:
-# 
-# 
+#
+#
 # Input: ["flower","flow","flight"]
 # Output: "fl"
-# 
-# 
+#
+#
 # Example 2:
-# 
-# 
+#
+#
 # Input: ["dog","racecar","car"]
 # Output: ""
 # Explanation: There is no common prefix among the input strings.
-# 
-# 
+#
+#
 # Note:
-# 
+#
 # All given inputs are in lowercase letters a-z.
-# 
+#
 #
 from typing import List
 
 # @lc code=start
+
+
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         if not strs or not len(strs):
             return ''
-        if len(strs)==1:
+        if len(strs) == 1:
             return strs[0]
         temp = []
         for c in strs[0]:
             temp.append(c)
 
-        for s in range(1,len(strs)):
+        for s in range(1, len(strs)):
             item = strs[s]
             i = 0
-            if len(temp)>len(item):
-                temp[len(item)]=None
-            for i in range(min(len(temp),len(item))):
+            if len(temp) > len(item):
+                temp[len(item)] = None
+            for i in range(min(len(temp), len(item))):
                 if not temp[i]:
                     break
                 if temp[i] != item[i]:
                     temp[i] = None
                     break
 
-        res=''
+        res = ''
         for t in temp:
             if not t:
                 break
-            res+=t
+            res += t
         return res
 
 # @lc code=end
+
 
 if __name__ == "__main__":
     i = 0
     for i in range(1):
         pass
     print(i)
-    s=Solution()
-    print(s.longestCommonPrefix(["abab","aba",'a']))
+    s = Solution()
+    print(s.longestCommonPrefix(["abab", "aba", 'a']))
