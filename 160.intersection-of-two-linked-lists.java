0f8ab9efa5ef0b501188bd1fa86aa2961b762665
Chenglong Ma <mcl.cn@outlook.com>
2019-11-19 11:08:45 +1100

    Q160 done in fast runtime but high memory usage

--- 160.intersection-of-two-linked-lists.java
+++ 160.intersection-of-two-linked-lists.java
@@ -101,16 +101,22 @@ public class ListNode {
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        Set<ListNode> set = new HashSet<>();
-        ListNode a = headA, b = headB;
-        while (a != null) {
-            set.add(a);
-            a = a.next;
+        if (headA == null || headB == null) {
+            return null;
         }
-        while (b != null) {
-            if (!set.add(b)) {
-                return b;
+
+        ListNode a = headA, b = headB;
+        while (a != null || b != null) {
+            if (a == null) {
+                a = headB;
             }
+            if (b == null) {
+                b = headA;
+            }
+            if (a == b) {
+                return a;
+            }
+            a = a.next;
             b = b.next;
         }
         return null;
