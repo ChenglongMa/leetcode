a8043909b47a0ab8e23de9d21c839433c50389b6
Chenglong Ma <mcl.cn@outlook.com>
2019-11-20 23:34:49 +1100

    Q167 done by two pointers

--- 167.two-sum-ii-input-array-is-sorted.py
+++ 167.two-sum-ii-input-array-is-sorted.py
@@ -15,36 +15,50 @@
 #
 # Given an array of integers that is already sorted in ascending order, find
 # two numbers such that they add up to a specific target number.
-# 
+#
 # The function twoSum should return indices of the two numbers such that they
 # add up to the target, where index1 must be less than index2.
-# 
+#
 # Note:
-# 
-# 
+#
+#
 # Your returned answers (both index1 and index2) are not zero-based.
 # You may assume that each input would have exactly one solution and you may
 # not use the same element twice.
-# 
-# 
+#
+#
 # Example:
-# 
-# 
+#
+#
 # Input: numbers = [2,7,11,15], target = 9
 # Output: [1,2]
 # Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
-# 
+#
 #
 
 # @lc code=start
+
+
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        i = 0
+        j = -1
+        length = len(numbers)
+        while True:
+            sum = numbers[i] + numbers[j]
+            if sum == target:
+                return [i + 1, length + j + 1]
+            if sum < target:
+                i += 1
+            else:
+                j -= 1
+
+    def twoSum2(self, numbers: List[int], target: int) -> List[int]:
         dic = {}
         for i in range(len(numbers)):
             if numbers[i] in dic:
                 return [dic[numbers[i]], i + 1]
             dic[target - numbers[i]] = i + 1
-        
-        
-# @lc code=end
 
+
+# @lc code=end
